f04bead0ec4487fa872cb05a2aa7841651776c3e
HANSIN <hansin.cho@emotion.co.kr>
2021-06-29 17:21:02 +0900

    초기

---
+++ src/main/java/com/example/how_to_update_attach/HowToUpdateAttachApplication.java
@@ -0,0 +1,13 @@
+package com.example.how_to_update_attach;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowToUpdateAttachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HowToUpdateAttachApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/how_to_update_attach/HowToUpdateAttachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.how_to_update_attach;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowToUpdateAttachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
